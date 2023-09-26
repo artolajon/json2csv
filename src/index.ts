ec97163ffadd64a2481cd8ad5dce74bd7134c18c
artolajon <artolajon2@gmail.com>
2023-09-26 22:10:14 +0200

    Design

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 import './style.scss';
-import image from './image.jpg';
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerText = "When you run the commands correctly"; 
-    element.classList.add('meme');
-
-    // Add the image to our existing div.
-    const myImage = new Image();
-    myImage.src = image;
-
-    element.appendChild(myImage);
-
-    return element;
+    
   }
-  document.body.innerHTML="";
-  document.body.appendChild(component());
