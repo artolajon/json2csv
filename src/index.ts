17f2a80a92ec939df89b890817fef6ec4cd2752e
artolajon <artolajon2@gmail.com>
2023-09-26 22:40:53 +0200

    Transform to csv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import './style.scss';
+import { json2csv } from 'json-2-csv';
 
-function component() {
-    
-  }
+let transformButton = document.getElementById("transform");
+transformButton.addEventListener("click", (event) => {
+
+  let inputArea: HTMLTextAreaElement = document.getElementById("input") as HTMLTextAreaElement;
+  let inputData = JSON.parse(inputArea.value);
+  
+  json2csv(inputData, {}).then(output =>{
+    (document.getElementById("output") as HTMLTextAreaElement).value = output;
+  });
+  
+});
