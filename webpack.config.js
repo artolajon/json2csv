705078bb762aff26844ca06f5aeb39a05062012e
artolajon <artolajon2@gmail.com>
2023-09-27 23:35:34 +0200

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -37,7 +38,7 @@ module.exports = {
     ],
   },
   plugins: [
-
+    new FaviconsWebpackPlugin('./src/favicon.ico'),
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
